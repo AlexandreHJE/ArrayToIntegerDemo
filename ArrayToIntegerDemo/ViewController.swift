add2e892c3f856306e8818fc7291f5039393a285
alexHu <alex.hu@opay.tw>
2021-09-08 14:13:22 +0800

    Initial Commit

---
+++ ArrayToIntegerDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//
+// ViewController.swift
+// ArrayToIntegerDemo
+//
+// Created by Alexandre Hu (HU JEN EN) on 2021/9/8
+// Copyright © 2021 Alexandre Hu. All rights reserved.
+//
+        
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ArrayToIntegerDemoTests/ArrayToIntegerDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//
+// ArrayToIntegerDemoTests.swift
+// ArrayToIntegerDemoTests
+//
+// Created by Alexandre Hu (HU JEN EN) on 2021/9/8
+// Copyright © 2021 Alexandre Hu. All rights reserved.
+//
+        
+
+import XCTest
+@testable import ArrayToIntegerDemo
+
+class ArrayToIntegerDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
