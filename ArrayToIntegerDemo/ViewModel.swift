17d2531b06c86c639584d873e5d97bd24f376555
alexHu <alex.hu@opay.tw>
2021-09-08 15:28:10 +0800

    Remove lines of strip-offed parsing logic from ViewModel

--- ArrayToIntegerDemo/ViewModel.swift
+++ ArrayToIntegerDemo/ViewModel.swift
@@ -9,6 +9,7 @@
 
 
 import Foundation
+import ArrayToIntegerParser
 
 protocol ViewModelDelegate: AnyObject {
     func viewModel(_ viewModel: ViewModel)
@@ -17,6 +18,7 @@ protocol ViewModelDelegate: AnyObject {
 final class ViewModel {
     
     weak var delegate: ViewModelDelegate?
+    private let parser: ArrayToIntegerParser = ArrayToIntegerParser()
     
     private(set) var model: Model = Model() {
         didSet {
@@ -30,122 +32,11 @@ final class ViewModel {
     
     private func updateModelContent(input: String) {
         let inputString = input
-        let integerFromInput = atoi_LC(input: input)
+        let integerFromInput = parser.atoi_LC(with: inputString)
         setupModel(with: inputString, integer: integerFromInput)
     }
     
     private func setupModel(with input: String, integer: Int) {
         model = Model(inputString: input, integerFromInput: integer)
-    }
-    
-    private func atoi_LC(input: String) -> Int {
-        var result = 0
-        var notSignedValue = 0
-        var signChanged: Bool = false
-        var isPositive: Bool = true
-        let inputArray = Array(input)
-        let trimmedArray = trimLeadingSpaceChars(array: inputArray)
-        let hasLeadingDigit = checkArrayHasLeadingDigit(array: inputArray)
-        outerLoop: for c in trimmedArray {
-            switch checkInputCharacterType(with: c) {
-                case .isSpaceChar:
-                    if signChanged || hasLeadingDigit {
-                        break outerLoop
-                    }
-
-                case .isPositiveSignChar:
-                    if hasLeadingDigit { break outerLoop }
-                    if !signChanged {
-                        signChanged = true
-                    } else {
-                        break outerLoop
-                    }
-
-                case .isNegativeSignChar:
-                    if hasLeadingDigit { break outerLoop }
-                    if !signChanged {
-                        signChanged = true
-                        isPositive = false
-                    } else {
-                        break outerLoop
-                    }
-
-                case .isNumericChar:
-                    notSignedValue = calculateValue(with: c, priviousValue: notSignedValue)
-                    if !isPositive {
-                        result = -(notSignedValue)
-                    } else {
-                        result = notSignedValue
-                    }
-                    
-                    if checkIfExceedInt32Bounds(with: result) {
-                        result = result > 0 ? Int.int32_max : Int.int32_min
-                        break outerLoop
-                    }
-
-                default:
-                    break outerLoop
-            }
-        }
-
-        return result
-    }
-    
-    private func checkArrayHasLeadingDigit(array: [Character]) -> Bool {
-        var result: Bool = false
-        for c in array {
-            if !c.isWhitespace && !(c.isASCII && c.isNumber) {
-                break
-            } else if (c.isASCII && c.isNumber) {
-                result = true
-                break
-            }
-        }
-        
-        return result
-    }
-
-    private func calculateValue(with addedDigit: Character, priviousValue: Int) -> Int {
-        var result = priviousValue
-        if addedDigit.isASCII && addedDigit.isNumber {
-            result = (result * 10) + (addedDigit.wholeNumberValue ?? 0)
-        }
-        
-        return result
-    }
-
-    private func trimLeadingSpaceChars(array: [Character]) -> [Character] {
-        var result: [Character] = []
-        var keepTrimmingFlag: Bool = true
-        for c in array {
-            if !c.isWhitespace {
-                keepTrimmingFlag = false
-            }
-            if !keepTrimmingFlag {
-                result.append(c)
-            }
-        }
-        
-        return result
-    }
-
-    private func checkIfExceedInt32Bounds(with value: Int) -> Bool {
-        return value > Int.int32_max || value < Int.int32_min
-    }
-
-    private func checkInputCharacterType(with char: Character) -> CharacterTypes {
-        var characterType: CharacterTypes = .other
-        
-        if char.isASCII && char.isNumber {
-            characterType = .isNumericChar
-        } else if char == "+" {
-            characterType = .isPositiveSignChar
-        } else if char == "-" {
-            characterType = .isNegativeSignChar
-        } else if char.isWhitespace {
-            characterType = .isSpaceChar
-        }
-        
-        return characterType
     }
 }
--- ArrayToIntegerDemoTests/ViewModelTests.swift
+++ ArrayToIntegerDemoTests/ViewModelTests.swift
@@ -12,43 +12,5 @@ import XCTest
 @testable import ArrayToIntegerDemo
 
 class ViewModelTests: XCTestCase {
-
-    private var viewModel = ViewModel()
-
-    func testInputIsPositiveAndNotExceededInt32Bound() {
-        let input: String = "5566"
-        let output = atoi_LeetCode(input: input)
-        XCTAssertEqual(output, 5566)
-    }
-    
-    func testInputIsNegativeAndNotExceededInt32Bound() {
-        let input: String = "-5566"
-        let output = atoi_LeetCode(input: input)
-        XCTAssertEqual(output, -5566)
-    }
-    
-    func testInputIsPositiveAndNotExceededInt32BoundButHaveLeadingElement() {
-        let input: String = "a5566"
-        let output = atoi_LC(input: input)
-        XCTAssertEqual(output, 0)
-    }
-    
-    func testInputIsPositiveAndNotExceededInt32BoundButHaveTrailingElement() {
-        let input: String = "5566a"
-        let output = atoi_LeetCode(input: input)
-        XCTAssertEqual(output, 5566)
-    }
-    
-    func testInputIsPositiveAndExceededInt32Bound() {
-        let input: String = "91283472332"
-        let output = atoi_LeetCode(input: input)
-        XCTAssertEqual(output, 2147483647)
-    }
-    
-    func testInputIsNegativeAndExceededInt32Bound() {
-        let input: String = "-91283472332"
-        let output = atoi_LeetCode(input: input)
-        XCTAssertEqual(output, -2147483648)
-    }
-
+    // 
 }
